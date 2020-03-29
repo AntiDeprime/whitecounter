764ed0d22de079e92d6754d502b8fcc2f76c697a
Aleksei <to@alxy.sh>
2020-03-29 19:11:44 +0300

    code readability improvements

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 # import graph and table
-import wc2
+from wc2 import WCAppData
 
-# import Dash components 
+# import Dash components
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import dash_table
 
-# Dash app configuration 
+# loading polar scatter plot and data table
+wc_app_data = WCAppData()
+plot = wc_app_data.build_scatter()
+table = wc_app_data.build_data_table()
+
+# Dash app configuration
 app = dash.Dash(
     __name__, external_stylesheets=[
-        # appearance 
+        # appearance
         "https://codepen.io/chriddyp/pen/bWLwgP.css",
-         ],
-        # social icons
-         external_scripts=[
+    ],
+    # social icons
+    external_scripts=[
         'https://kit.fontawesome.com/6b0b673ac1.js',
-         ]
+    ]
 )
 server = app.server
 app.title = 'White Counter'
@@ -25,87 +30,94 @@ app.title = 'White Counter'
 # App layout
 app.layout = html.Div(id='container', children=[
     # Header
-    html.H1(children=['Protests in Moscow, White Counter Historical Data'], id='app-header',),
-    # Tabs 
+    html.H1(children=[('Protests in Moscow, '
+                       'White Counter Historical Data')],
+            id='app-header',),
+    # Tabs
     dcc.Tabs([
         # Graph tab
         dcc.Tab(label='Plot', id='graph', children=[
-            dcc.Graph(id="polar_plot", config={'displayModeBar': False}, figure=wc2.fig,),
+            dcc.Graph(id="polar_plot", config={
+                      'displayModeBar': False}, figure=plot,),
         ]),
-        
+
         # Data Table
         dcc.Tab(label='Data Table', children=[
 
             dash_table.DataTable(
-                # loading data 
-                data=wc2.show_df.to_dict('records'),
-                columns=[{'id': c, 'name': c} for c in wc2.show_df.columns],
+                # loading data
+                data=table.to_dict('records'),
+                columns=[{'id': c, 'name': c} for c in table],
                 # fixed header
-                fixed_rows={ 'headers': True, 'data': 0 },
-                
+                fixed_rows={'headers': True, 'data': 0},
+
                 # font, wrapping, align
-                style_cell = {
+                style_cell={
                     'font-family': 'PT Sans',
                     'font-size': '1.4rem',
                     'whiteSpace': 'normal',
                     'height': 'auto',
                     'maxWidth': 0,
                     'textAlign': 'center'},
-                
-                # widths 
+
+                # widths
                 style_cell_conditional=[
                     {'if': {'column_id': 'Date'},
-                        'width': '15%',},
+                        'width': '15%', },
                     {'if': {'column_id': 'Attendance'},
-                        'width': '15%',},
+                        'width': '15%', },
                     {'if': {'column_id': 'Location'},
-                        'width': '35%',},
+                        'width': '35%', },
                 ],
-                
+
                 # odd rows grey
                 style_data_conditional=[
                     {'if': {'row_index': 'odd'},
                         'backgroundColor': 'rgb(248, 248, 248)'}
                 ],
-                
-                # format header 
+
+                # format header
                 style_header={
                     'backgroundColor': 'rgb(230, 230, 230)',
                     'fontWeight': 'bold',
                     'textAlign': 'center',
                 },
-                
-                # no vertical borders 
-                style_as_list_view=True,        
-                
-                ) ,
-            ]),
+
+                # no vertical borders
+                style_as_list_view=True,
+
+            ),
         ]),
+    ]),
 
-
+    # Footer
     html.Div([
         html.Br(),
-        html.P(['Developed by Aleksei Shchetinin', 
-                    html.Br(),
-                    html.A([html.I(className='far fa-envelope')], href='mailto:to@alxy.sh'),
-                    ' ',
-                    html.A([html.I(className='fab fa-github')], href='https://github.com/AntiDeprime'),
-                    ' ',
-                    html.A([html.I(className='fab fa-telegram')], href='https://t.me/antideprime'),
-                    html.Br(),
+        html.P(['Developed by Aleksei Shchetinin',
+                html.Br(),
+                html.A([html.I(className='far fa-envelope')],
+                       href='mailto:to@alxy.sh'),
+                ' ',
+                html.A([html.I(className='fab fa-github')],
+                       href='https://github.com/AntiDeprime'),
+                ' ',
+                html.A([html.I(className='fab fa-telegram')],
+                       href='https://t.me/antideprime'),
+                html.Br(),
 
                 ]),
 
-        html.P(['White Counter NGO', 
-            html.Br(),
-            html.A([html.I(className='fab fa-twitter')], href='https://twitter.com/WhiteCounter'),
-            ' ',
-            html.A([html.I(className='fab fa-facebook')], href='https://facebook.com/WhiteCounter'),
-        ]),
-        html.I(['Last update: 2020/03/12']),
+        html.P(['White Counter NGO',
+                html.Br(),
+                html.A([html.I(className='fab fa-twitter')],
+                       href='https://twitter.com/WhiteCounter'),
+                ' ',
+                html.A([html.I(className='fab fa-facebook')],
+                       href='https://facebook.com/WhiteCounter'),
+                ]),
+        html.I(['Last update: 2020/03/29']),
     ], id='source',)
 ])
-
 
 if (__name__ == '__main__'):
     app.run_server(debug=True)
--- wc2.py
+++ wc2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -11,86 +5,89 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-# In[8]:
+class WCAppData(object):
+    '''Performs data loading and processing,
+       Builds plot and data table'''
 
+    def __init__(self):
+        # set month names
+        self.months = ['January', 'February', 'March',
+                       'April', 'May', 'June', 'July',
+                       'August', 'September', 'October',
+                       'November', 'December']
+        #set color scheme
+        self.colors = px.colors.sequential.Viridis[2:6]
+        #load data 
+        self.load_data('data.csv')
 
-df = pd.read_csv('data.csv', parse_dates=['date'])
-df.dropna(subset = ['count'], inplace=True)
+    def __polar_days__(self, row):
+        '''Distributes dates evenly across 360 degrees'''
+        if (row.date.year % 4 == 0):
+            return (row.date.dayofyear / 366 * 360)
+        else:
+            return (row.date.dayofyear / 365 * 360)
 
+    def load_data(self, path):
+        '''Loads data frame'''
+        self.df = pd\
+            .read_csv(path, parse_dates=['date'])\
+            .dropna(subset=['count'])\
+            .sort_values(['date'], ascending=False)
+        
+        # make evenly distributed dates
+        self.df['polar_date'] = self.df.apply(
+            lambda row: self.__polar_days__(row), axis=1)
+        
+        self.df['year'] = pd.Categorical(self.df.date.dt.year)
+        # format dates
+        self.df['date'] = self.df.date.dt.strftime('%Y-%m-%d')
+        # make counts whole numbers
+        self.df['count'] = self.df['count'].astype(int)
 
-# In[9]:
+    def build_scatter(self):
+        '''Builds scatter polar plot'''
+        fig = px.scatter_polar(
+            self.df,
+            r="year",
+            theta="polar_date",
+            color=np.sqrt(self.df['count']),
+            size=np.sqrt(self.df['count']),
+            color_continuous_scale=self.colors,
+            size_max=30, # appearance
+            range_r=[2011, 2020],  # appearance
+            custom_data=['date', 'name', 'count'])  # for hover
 
+        fig.update_layout(
 
-# Distribute years evenly across 360 degrees 
-def polar_days(row):
-    if (row.date.year % 4 == 0):
-        return (row.date.dayofyear / 366 * 360)
-    else:
-        return (row.date.dayofyear / 365 * 360)
+            template=None,
+            font=dict(size=18,),
 
-df['polar_date'] = df.apply(lambda row: polar_days(row), axis=1)
-df['year'] = pd.Categorical(df.date.dt.year)
-df = df.sort_values(['year', 'polar_date'])
+            # format axes
+            polar=dict(angularaxis=dict(
+                tickvals=[(a*30) for a in range(0, 12)],
+                ticktext=self.months),
+                radialaxis=dict(
+                tickvals=[2013, 2014, 2015, 2016,
+                          2017, 2018, 2019, 2020],
+                angle=90,
+                tickangle=90,),),
 
+            # format colorbar
+            coloraxis_colorbar=dict(
+                title='Attendance',
+                ticks="outside",
+                tickvals=np.sqrt([1000, 10000, 25000, 50000]),
+                ticktext=['1k', '10k', '25k', '50k'],),)
 
-# In[10]:
+        # format hover
+        fig.update_traces(
+            hovertemplate=('<b>%{customdata[1]}</b><br>'
+                           'Date: %{customdata[0]}<br>'
+                           'Attendance: %{customdata[2]}'),)
+        return(fig)
 
-
-# Build scatter polar plot 
-
-fig = px.scatter_polar(df, r="year", theta="polar_date", # data
-                       color=np.sqrt(df['count']), size=np.sqrt(df['count']), #size and color 
-                       color_continuous_scale = px.colors.sequential.Viridis[2:6],size_max=30, # appearance 
-                       range_r=[2011, 2020],  # appearance 
-                       custom_data=[df.date.dt.strftime('%Y-%m-%d'), 'name', 'count']) # for hover 
-
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-          'August', 'September', 'October', 'November', 'December'] 
-
-
-fig.update_layout(
-    template=None,
-    # font size
-    font=dict(size=18,),
-    
-    # format axes
-    polar = dict(
-        angularaxis = dict(tickvals=[(a*30) for a in range(0, 12)], ticktext=months), 
-        radialaxis = dict(tickvals=[2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
-                          angle = 90,
-                          tickangle = 90,),
-    ),
-    
-    # format colorbar 
-    coloraxis_colorbar=dict(
-        title='Attendance',
-        ticks="outside",
-        tickvals=np.sqrt([1000, 10000, 25000, 50000]),
-        ticktext=['1k', '10k', '25k', '50k'],  
-    ),
-)
-
-# format hover 
-fig.update_traces(
-    hovertemplate='<b>%{customdata[1]}</b><br>Date: %{customdata[0]}<br>Attendance: %{customdata[2]}',
-)
-
-
-# In[11]:
-
-
-# creating data table 
-# sorting descending by date 
-df.sort_values(['date'], ascending=False, inplace=True)
-
-show_df = df[['date', 'name', 'count', 'loc']]
-
-show_df = pd.DataFrame([df['name'], df.date.dt.strftime('%Y-%m-%d'), df['count'], df['loc']]).T
-show_df.columns = ['Name', 'Date', 'Attendance', 'Location']
-
-
-# In[ ]:
-
-
-
-
+    def build_data_table(self):
+        '''Builds a table to show in the app'''
+        out = self.df[['name', 'date', 'count', 'loc']].copy()
+        out.columns = ['Name', 'Date', 'Attendance', 'Location']
+        return(out)
